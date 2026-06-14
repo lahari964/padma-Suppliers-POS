0a76a46e38f64fd5f4d74906923dd9e7986af58b
lahari Varikunta <lahari@LaharisAir.local>
2026-06-15 02:54:41 +0530

    chore: remove old unused backup files and scripts

--- build-check.js
+++
@@ -1,7 +0,0 @@
-import { execSync } from 'child_process';
-try {
-  console.log(execSync('npm run build', { encoding: 'utf-8' }));
-} catch (e) {
-  console.error(e.stdout);
-  console.error(e.stderr);
-}
